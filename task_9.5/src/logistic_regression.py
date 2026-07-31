26731dda537adc66f152094ec69f7aaa15fede69
unknown <dhruvam.mitmpl2025@learner.manipal.edu>
2026-07-31 18:06:50 +0530

    initialising logistic reg

---
+++ task_9.5/src/logistic_regression.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class LogisticRegression: 
+    def _init_():
+        pass
+
+    def predict():
+        pass
+
+    def compute_cost():
+        pass
+
+    def compute_gradients():
+        pass
+
+    def fit():
+        pass
